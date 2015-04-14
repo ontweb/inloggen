1424c1cfd3b37529e9d8a7edaa69cef62591c8c7
ontweb <xanderv@zeelandnet.nl>
2015-04-14 15:23:22 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//Sessie starten zodat het login systeem werkt
+	$session_version=432; 
+	session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" 
+           href="style.css"/>
+           <style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
+	</head>
+	<body>
+		<?php include 'header.php' ?>
+		<center><h1>Opstellen jaarrekening</h1></center>
+	</body>
+</html>
+ 
